3cebd9206988991fd8e63df1463165b35aa28eb6
kpermenter <52431178+kpermenter@users.noreply.github.com>
2022-07-26 14:39:12 -0500

    Add files via upload

--- src/components/CreateExpense/CreateExpense.tsx
+++ src/components/CreateExpense/CreateExpense.tsx
@@ -18,14 +18,9 @@ export const FormDialog = () => {
     createModalOpen,
   } = expenseLogs;
   
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
   const handleClose = () => {
-    setOpen(false);
+    /* @ts-ignore */
+    updateContextValue('createModalOpen', false);
   };
 
   return (
